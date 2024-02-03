15b44e89e5b09c437d098ac733f1d8cf99c2105c
sherbie00 <157928201+sherbie00@users.noreply.github.com>
2024-02-03 13:35:37 +0000

    Defined my Network Matrix as an Array

--- Floyd-Warshall Algorithm with Recursion.py
+++ Floyd-Warshall Algorithm with Recursion.py
@@ -8,4 +8,10 @@ INF = 9999
 
 # Defining my matrix as an array
 
-distance = []
+network_matrix = {
+    0: {0: 0,1: 4,2:1 ,3: INF,4:INF},
+    1: {0: INF, 1: 0, 2: INF, 3: 4, 2: 2},
+    2: {0: 1,1: INF,2: 0,3: 2,4: INF},
+    3: {0: 4,1: 1,2: INF,3: 0,4: 3},
+    4: {0: INF,1: 3,2: INF,3: 1,4: 0}
+}
