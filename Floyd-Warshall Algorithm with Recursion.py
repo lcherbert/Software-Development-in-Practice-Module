5bbd11ad0deb696964a066ed04b0c52f4f918318
sherbie00 <157928201+sherbie00@users.noreply.github.com>
2024-02-03 18:58:44 +0000

    Changed Array to Matrix and reduced to 4 vertices. Added print functionality to check matrix.

--- Floyd-Warshall Algorithm with Recursion.py
+++ Floyd-Warshall Algorithm with Recursion.py
@@ -1,29 +1,29 @@
 # Floyd-Warshall Algorithm with Recursion
 
 # Number of Vertices in my Network Graph
-V = 5
+V = 4
 
 # Defining the 'infinite' value
 INF = 9999
 
 # Defining the network matrix as an array
-network_matrix = {
-    0: {0: 0,1: 4,2:1 ,3: INF,4:INF},
-    1: {0: INF, 1: 0, 2: INF, 3: 4, 2: 2},
-    2: {0: 1,1: INF,2: 0,3: 2,4: INF},
-    3: {0: 4,1: 1,2: INF,3: 0,4: 3},
-    4: {0: INF,1: 3,2: INF,3: 1,4: 0}
-}
+network_matrix = [
+    [0, 3, 1, INF],
+    [INF, 0, INF, 2],
+    [1, INF, 0, 2],
+    [3, 1, INF, 0],
+]
 
 # Non-Recursive Floyd-Warshall Algorithm Function
 
 # Calling the defined universal variable V into the function
 def floydwarshall(matrix,V):
 
-    # using V-1 as Python starts at 0 and not at 1
-    for k in range(V-1):
-        for i in range(V-1):
-            for j in range(V-1):
+    distance = [row[:] for row in matrix]
+
+    for k in range(V):
+        for i in range(V):
+            for j in range(V):
                 matrix[i][j] = min(matrix[i][j], matrix[i][k] + matrix[k][j])
 
     return matrix
