d0086f5b1fa2a8d7ca3f01b466ecef37a2bd995d
Louise Charlesworth-Herbert <157928201+sherbie00@users.noreply.github.com>
2024-02-14 21:07:30 +0000

    Adding the Recursive Algorithm

--- Floyd-Warshall Algorithm with Recursion.py
+++ Floyd-Warshall Algorithm with Recursion.py
@@ -21,6 +21,7 @@ def floydwarshall(matrix,V):
 
     distance = [row[:] for row in matrix]
 
+# Implement the Algorithm
     for k in range(V):
         for i in range(V):
             for j in range(V):
@@ -28,7 +29,39 @@ def floydwarshall(matrix,V):
 
     return matrix
 
-output = floydwarshall(network_matrix, V)
+nonrecursive_output = floydwarshall(network_matrix, V)
 
-for row in output:
+# Print the None-Recursive Algorithm
+print("The Floyd-Warshall Algorithm Output without recursion:")
+for row in nonrecursive_output:
+    print(row)
+
+# Recursive Floyd-Warshall Algorithm Function
+    
+# Calling the previously defined variables into the function
+def floydwarshallrecursion(matrix, V, k, i, j):
+    if k >= V:
+        return matrix
+    
+# Check the distances between the nodes in the network (represented by i, j, and k)
+    if matrix[i][j] > matrix[i][k] + matrix[k][j]:
+        matrix[i][j] = matrix[i][k] + matrix[k][j]
+
+
+# Ensures all nodes have been processed in the algorithm
+    if j < V - 1:
+        return floydwarshallrecursion(matrix, V, k, i, j + 1)
+    elif i < V - 1:
+        return floydwarshallrecursion(matrix, V, k, i + 1, 0)
+    else:
+        return floydwarshallrecursion(matrix, V, k + 1, 0, 0)
+
+def floydwarshall(matrix, V):
+    return floydwarshallrecursion(matrix, V, 0, 0, 0)
+
+recursive_output = floydwarshall(network_matrix, V)
+
+# Print the Recursive Implementation of the Algorithm
+print("The Floyd-Warshall Algorithm with recursion:")
+for row in recursive_output:
     print(row)
