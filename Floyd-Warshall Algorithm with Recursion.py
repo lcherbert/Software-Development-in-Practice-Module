d827618d47613732666b0fe6c5f0d394707e4e48
Louise Charlesworth-Herbert <157928201+lcherbert@users.noreply.github.com>
2024-02-18 23:07:51 +0000

    Added a Counter for Iterations and Recursions

--- Floyd-Warshall Algorithm with Recursion.py
+++ Floyd-Warshall Algorithm with Recursion.py
@@ -18,10 +18,15 @@ matrix = [
     [3, 1, INF, 0],
 ]
 
+iterative_counter = 0
+
+recursive_counter = 0
+
 # Non-Recursive Floyd-Warshall Algorithm Function
 
 # Calling the defined universal variable V into the function
 def floydwarshalliterative(matrix,V):
+    global iterative_counter
 
     distance = [row[:] for row in matrix]
 
@@ -29,22 +34,25 @@ def floydwarshalliterative(matrix,V):
     for k in range(V):
         for i in range(V):
             for j in range(V):
+                iterative_counter = iterative_counter + int(1)
                 matrix[i][j] = min(matrix[i][j], matrix[i][k] + matrix[k][j])
 
-    return matrix
+    return (matrix,iterative_counter)
 
 
 # Recursive Floyd-Warshall Algorithm Function
     
 # Calling the previously defined variables into the function
 def floydwarshallrecursion(matrix, V, k, i, j):
+    global recursive_counter
+
+    recursive_counter = recursive_counter + int(1)
     if k >= V:
-        return matrix
+        return (matrix)
     
 # Check the distances between the nodes in the network (represented by i, j, and k)
     if matrix[i][j] > matrix[i][k] + matrix[k][j]:
         matrix[i][j] = matrix[i][k] + matrix[k][j]
-
 
 # Ensures all nodes have been processed in the algorithm
     if j < V - 1:
@@ -58,6 +66,23 @@ def floydwarshallrecursion(matrix, V, k, i, j):
 def algorithm(matrix, V):
     return floydwarshallrecursion(matrix, V, 0, 0, 0)
 
+nonrecursive_output = floydwarshalliterative(matrix, V)
+
+# Print the None-Recursive Algorithm
+print("The Floyd-Warshall Algorithm Output without recursion:")
+for row in nonrecursive_output:
+    print(row)
+print("Number of Iterations: ", iterative_counter)
+
+# Assign the output to the variable
+recursive_output = algorithm(matrix, V)
+
+# Print the Recursive Implementation of the Algorithm
+print("The Floyd-Warshall Algorithm with recursion:")
+for row in recursive_output:
+    print(row)
+print("Number of Recursions: ", recursive_counter)
+
 class TestFloydWarshall(unittest.TestCase):
     def test_floyd_washall(self):
         expected_output = [
@@ -69,21 +94,6 @@ class TestFloydWarshall(unittest.TestCase):
         
         actual_output = algorithm(matrix, V)
         self.assertEqual(actual_output, expected_output)
-
-        nonrecursive_output = floydwarshalliterative(floydwarshalliterative(matrix, V),V)
-
-# Print the None-Recursive Algorithm
-        print("The Floyd-Warshall Algorithm Output without recursion:")
-        for row in nonrecursive_output:
-            print(row)
-
-# Assign the output to the variable
-        recursive_output = algorithm(matrix, V)
-
-# Print the Recursive Implementation of the Algorithm
-        print("The Floyd-Warshall Algorithm with recursion:")
-        for row in recursive_output:
-            print(row)
 
 if __name__ == '__main__':
     unittest.main()
